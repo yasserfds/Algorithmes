27dd8afa2c23c8819939b28cdde3dbc13543db3d
TheGiantNinja <yasserfedsi34@gmail.com>
2022-12-06 19:20:34 +0100

    Turned js to python

---
+++ PythonAlgorithmes/Problem-solving/switch.py
@@ -0,0 +1,18 @@
+""" Question is we have two variables x = 10 and y = 5 and we want to change the value
+of both variables to be x = 5 and y = 10 how can we do that without third variable?
+
+Answer:
+
+"""
+
+x = 10
+
+y = 5
+
+x = x + y
+
+y = x - y
+
+x = x - y
+
+print("x = ", x, "y = ", y)
