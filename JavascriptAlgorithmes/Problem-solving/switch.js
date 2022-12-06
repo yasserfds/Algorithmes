a58afe787d2c923ad767c85054af066f840225ed
TheGiantNinja <yasserfedsi34@gmail.com>
2022-12-06 19:16:30 +0100

    Done

--- JavascriptAlgorithmes/Problem-solving/switch.js
+++ JavascriptAlgorithmes/Problem-solving/switch.js
@@ -1,3 +1,8 @@
+// Question is we have two variables x = 10 and y = 5 and we want to change the value
+// of both variables to be x = 5 and y = 10 how can we do that without third variable?
+
+// Answer:
+
 let x = 10, y = 5
 
 x = x + y
