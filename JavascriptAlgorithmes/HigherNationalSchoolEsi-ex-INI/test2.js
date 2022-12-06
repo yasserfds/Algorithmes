6eaeba7aba70aeacdd7dac38761360ed48fd3f90
TheGiantNinja <73761819+yasserfds@users.noreply.github.com>
2022-12-06 18:22:30 +0100

    Update test2.js

--- JavascriptAlgorithmes/HigherNationalSchoolEsi-ex-INI/test2.js
+++ JavascriptAlgorithmes/HigherNationalSchoolEsi-ex-INI/test2.js
@@ -3,4 +3,12 @@ var y = -10.80
 var z = x * y
 var y = z / 2
 var z = x - y / x
+
+if (z == x * y) {
+  console.log(x, y, z)
+} else {
+  if (z == x - y / x) {
+    console.log(x, y, z)
+  }
+}
 console.log(x, y, z)
