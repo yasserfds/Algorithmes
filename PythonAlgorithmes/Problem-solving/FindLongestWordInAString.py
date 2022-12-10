8ec86519bee693c303243001d185c713adf05ca3
TheGiantNinja <yasserfedsi34@gmail.com>
2022-12-10 23:25:12 +0100

    First solution is done

---
+++ PythonAlgorithmes/Problem-solving/FindLongestWordInAString.py
@@ -0,0 +1,26 @@
+# First solution
+
+"""def longest_word_in(sentence):
+    count = 0
+    word_list = sentence.split("  ")
+    for word in word_list:
+        if len (word) > count:
+            count = len(word)
+            longest = word
+
+    return longest """
+
+# Second solution
+
+def longest_word_in(sentence):
+    longest = ""
+    word_list = sentence.split("  ")
+    
+
+
+
+
+
+# Testing Output
+# Toooooo
+print(longest_word_in("In Programming We Love Elzero Academy tooooooo much"))
