c112bf9359399ff927447e086bc49d2ddf260728
TheGiantNinja <yasserfedsi34@gmail.com>
2022-12-10 23:45:48 +0100

    First solution is done

--- JavascriptAlgorithmes/Problem-solving/FindLongestWordInAString.js
+++ JavascriptAlgorithmes/Problem-solving/FindLongestWordInAString.js
@@ -1,5 +1,28 @@
-function longestWordIn(sentence) {
+// First solution:
+
+/* function longestWordIn(sentence) {
     let splArray = sentence.split("  ");
+    let longestLen = 0;
+    let longestWord;
+
+
+    for (let i = 0; i < splArray.length; i++) {
+        if (splArray[1].length > longestLen) {
+            longestLen = splArray[i];
+            longestLen = splArray[i].length;
+        }
+    }
+
+    return splArray;
+} */
+
+// Second solution using rediuce
+
+function longestWordIn(sentence) {
+    let longestWord = sentence.split("  ").reduce(function(longest, current) {
+        return current.legth > longest.length ? current : longest;
+    })
+
     return splArray;
 }
 
