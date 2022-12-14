85bdbb1b60f18f16ddaadf7dae9d4a6979e8a7cd
TheGiantNinja <yasserfedsi34@gmail.com>
2022-12-14 19:27:43 +0100

    Problem solved

--- PythonAlgorithmes/Problem-solving/Snail-in-the-well.py
+++ PythonAlgorithmes/Problem-solving/Snail-in-the-well.py
@@ -1,3 +1,13 @@
 def daysCount(depth, height):
-    if (height >= depth):
+    if height >= depth:
         return 0
+    else:
+        if height + 7 >= depth:
+            return 1 + daysCount(depth, height + 7)
+        else:
+            return 1 + daysCount(depth, height + 5)
+
+
+def main():
+    depth = parseInt(readLine(), 10)
+    print(daysCount(depth, 0))
